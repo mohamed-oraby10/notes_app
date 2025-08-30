822a03095028b49e337f9fd4661c0b4da16f119d
hp <mohamedoraby0500@gmail.com>
2025-08-30 16:26:47 +0300

    pick color

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
@@ -6,16 +7,17 @@ import 'package:notes_app/models/note_model.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-
+  Color color = Color(0xffAC3931);
   addNote(NoteModel note) async {
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNoteBox);
-      
+
       await notesBox.add(note);
       emit(AddNoteSuccess());
     } catch (e) {
-     emit(AddNoteFailure(e.toString())) ;
+      emit(AddNoteFailure(e.toString()));
     }
   }
 }
--- lib/widgets/colors_list_view.dart
+++ lib/widgets/colors_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
@@ -29,9 +31,13 @@ class _ColorsListViewState extends State<ColorsListView> {
             child: GestureDetector(
               onTap: () {
                 currentIndex = index;
+                BlocProvider.of<AddNoteCubit>(context).color = colors[index];
                 setState(() {});
               },
-              child: ColorItem(isSelected: currentIndex == index, color: colors[index],),
+              child: ColorItem(
+                isSelected: currentIndex == index,
+                color: colors[index],
+              ),
             ),
           );
         },
